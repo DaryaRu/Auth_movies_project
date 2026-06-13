b6018be914ced1cf55a4da36a7e892ed2635edf0
Evgenia <eug.gross@yandex.ru>
2026-06-13 20:02:48 +0300

    add tests

--- tests/functional/src/test_auth.py
+++ tests/functional/src/test_auth.py
@@ -286,3 +286,172 @@ class TestLogout:
         )
 
         assert_error_detail(data)
+        
+        
+class TestLogoutAll:
+    LOGOUT_ALL_URL = f"{test_settings.api_prefix}/logout-all/"
+    LOGIN_URL = f"{test_settings.api_prefix}/login/"
+    ACTIVE_SESSION_URL = f"{test_settings.api_prefix}/active_sessions/"
+    
+    async def test_logout_all_success(
+        self,
+        cookie_http_client: ClientSession,
+        active_user_data: dict[str, Any],
+    ) -> None:
+        """Успешный выход возвращает 204 и удаляет refresh_token из БД."""
+        login_response_1 = await cookie_http_client.post(
+            self.LOGIN_URL,
+            json={
+                "email": active_user_data["email"],
+                "password": active_user_data["password"],
+            },
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/137.0.0.0 Safari/537.36"
+                )
+            }
+        )
+        login_response_2 = await cookie_http_client.post(
+            self.LOGIN_URL,
+            json={
+                "email": active_user_data["email"],
+                "password": active_user_data["password"],
+            },
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (iPhone; CPU iPhone OS 18_3 like Mac OS X) "
+                    "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                    "Version/18.3 Mobile/15E148 Safari/604.1"
+                )
+            }
+        )
+        login_data_1 = await login_response_1.json()
+        access_token_1 = login_data_1["access_token"]
+
+        login_data_2 = await login_response_2.json()
+        access_token_2 = login_data_2["access_token"]
+
+        response = await cookie_http_client.post(self.LOGOUT_ALL_URL)
+        await assert_status(response, HTTPStatus.NO_CONTENT)
+        
+        session_response_1 = await cookie_http_client.get(
+            self.ACTIVE_SESSION_URL,
+            headers = {"Authorization": f"Bearer {access_token_1}"}
+        )
+        await assert_status(session_response_1, HTTPStatus.UNAUTHORIZED)
+        session_response_2 = await cookie_http_client.get(
+            self.ACTIVE_SESSION_URL,
+            headers = {"Authorization": f"Bearer {access_token_2}"}
+        )
+        await assert_status(session_response_2, HTTPStatus.UNAUTHORIZED)
+        
+    async def test_logout_all_without_token(
+        self,
+        http_client: ClientSession,
+    ) -> None:
+        """Запрос выхода без cookie возвращает 401."""
+        response = await http_client.post(self.LOGOUT_ALL_URL)
+        data = await assert_status_return_json(
+            response, HTTPStatus.UNAUTHORIZED
+        )
+
+        assert_error_detail(data)
+        
+
+class TestActiveSession:
+    LOGIN_URL = f"{test_settings.api_prefix}/login/"
+    ACTIVE_SESSION_URL = f"{test_settings.api_prefix}/active_sessions/"
+    LOGOUT_URL = f"{test_settings.api_prefix}/logout/"
+    
+    async def test_get_active_session_success(
+        self,
+        http_client: ClientSession,
+        active_user_data: dict[str, Any],
+        flush_redis_db: None,
+    ) -> None:
+        login_response = await http_client.post(
+            self.LOGIN_URL,
+            json={
+                "email": active_user_data["email"],
+                "password": active_user_data["password"],
+            },
+        )
+        login_data = await login_response.json()
+        access_token = login_data["access_token"]
+        active_session_response = await http_client.get(
+            self.ACTIVE_SESSION_URL,
+            headers = {"Authorization": f"Bearer {access_token}"}
+        )
+        data = await assert_status_return_json(active_session_response, HTTPStatus.OK)
+        assert len(data) == 1
+        assert "user_agent" in data[0]
+        assert "ip" in data[0]
+        assert "created_at" in data[0]
+        assert "is_current" in data[0]
+        
+    async def test_change_in_active_sessions(
+        self,
+        cookie_http_client: ClientSession,
+        active_user_data: dict[str, Any],
+        flush_redis_db: None,
+    ) -> None:
+        login_response_1 = await cookie_http_client.post(
+            self.LOGIN_URL,
+            json={
+                "email": active_user_data["email"],
+                "password": active_user_data["password"],
+            },
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/137.0.0.0 Safari/537.36"
+                )
+            }
+        )
+        login_response_2 = await cookie_http_client.post(
+            self.LOGIN_URL,
+            json={
+                "email": active_user_data["email"],
+                "password": active_user_data["password"],
+            },
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (iPhone; CPU iPhone OS 18_3 like Mac OS X) "
+                    "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                    "Version/18.3 Mobile/15E148 Safari/604.1"
+                )
+            }
+        )
+        login_data_1 = await login_response_1.json()
+        access_token_1 = login_data_1["access_token"]
+
+        login_data_2 = await login_response_2.json()
+        access_token_2 = login_data_2["access_token"]
+        active_session_response = await cookie_http_client.get(
+            self.ACTIVE_SESSION_URL,
+            headers = {"Authorization": f"Bearer {access_token_2}"}
+        )
+        data = await assert_status_return_json(active_session_response, HTTPStatus.OK)
+        assert len(data) == 2
+        assert sum(item["is_current"] for item in data) == 1
+        logout_response = await cookie_http_client.post(self.LOGOUT_URL)
+        await assert_status(logout_response, HTTPStatus.NO_CONTENT)
+        active_session_response = await cookie_http_client.get(
+            self.ACTIVE_SESSION_URL,
+            headers = {"Authorization": f"Bearer {access_token_1}"}
+        )
+        data = await assert_status_return_json(active_session_response, HTTPStatus.OK)
+        assert len(data) == 1
+
+    async def test_get_active_session_error_without_headers(
+        self,
+        http_client: ClientSession,
+    ) -> None:
+        response = await http_client.get(
+            self.ACTIVE_SESSION_URL,
+        )
+        data = await assert_status_return_json(response, HTTPStatus.UNAUTHORIZED)
+        assert_error_detail(data)
