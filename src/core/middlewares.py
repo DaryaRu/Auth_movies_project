f9eed2de4ad1fadebb6a4d6b93e81b298b9dc83e
Evgenia <eug.gross@yandex.ru>
2026-06-21 13:47:51 +0300

    add-trace

--- src/core/middlewares.py
+++ src/core/middlewares.py
@@ -1,9 +1,10 @@
 import logging
 import time
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.responses import JSONResponse
 from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from src.core.config import settings
@@ -11,20 +12,30 @@ from src.core.config import settings
 
 def register_middlewares(app: FastAPI) -> None:
     """Зарегистрировать все middleware в приложении"""
+    
+    @app.middleware('http')
+    async def check_request_id(request: Request, call_next):
+        request_id = request.headers.get('X-Request-Id')
+        if not request_id and request.url.path != "/health":
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': {'error': 'X-Request-Id is required'}})
+        response = await call_next(request)
+        return response
 
     @app.middleware("http")
     async def add_process_time_header(request: Request, call_next):
         """Middleware для измерения времени выполнения запроса"""
         start_time = time.perf_counter()
-        response = await call_next(request)
-        process_time = time.perf_counter() - start_time
-        response.headers["X-Process-Time"] = str(process_time)
-        logging.info(
-            f"Request: {request.method} {request.url.path} "
-            f"Completed in {process_time:.4f} seconds "
-            f"Status: {response.status_code}"
-        )
-        return response
+        try:
+            response = await call_next(request)
+            return response
+        finally:
+            process_time = time.perf_counter() - start_time
+            response.headers["X-Process-Time"] = str(process_time)
+            logging.info(
+                f"Request: {request.method} {request.url.path} "
+                f"Completed in {process_time:.4f} seconds "
+                f"Status: {response.status_code}"
+            )
 
     app.add_middleware(
         TrustedHostMiddleware, allowed_hosts=settings.ALLOWED_HOSTS.split(",")
