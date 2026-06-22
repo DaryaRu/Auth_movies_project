6e657eccf0e0a7b8569c3aa0ce5e29fd5d884ff1
DaryaRu <fonarina57@gmail.com>
2026-06-22 13:52:57 +0300

    fix: raise ProviderException

--- src/integrations/oauth/vk_provider.py
+++ src/integrations/oauth/vk_provider.py
@@ -1,10 +1,11 @@
+from src.exceptions import ProviderException
 from src.integrations.oauth.base_provider import OAuthBaseProvider
 from src.schemas.oauth import OAuthUserInfoScheme
 
 
 class VkOAuthProvider(OAuthBaseProvider):
-    async def get_auth_url(self, state: str) -> str:
-        ...
+    def get_auth_url(self, state: str) -> str:
+        raise ProviderException(detail="VK провайдер не поддерживается")
 
     async def get_user_info(self, code: str) -> OAuthUserInfoScheme:
-        ...
+        raise ProviderException(detail="VK провайдер не поддерживается")
