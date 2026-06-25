ca1e239e35de3120b2d8cdfee506c250a15fb37d
DaryaRu <fonarina57@gmail.com>
2026-06-25 16:54:50 +0300

    feat: add subscription repository

--- auth/src/utils/db_manager.py
+++ auth/src/utils/db_manager.py
@@ -1,6 +1,7 @@
 from src.repositories.oauth_accounts import OAuthAccountsPostgreSQLRepository
 from src.repositories.permissions import PermissionsPostgreSQLRepository
 from src.repositories.roles import RolesPostgreSQLRepository
+from src.repositories.subscriptions import SubscriptionsPostgreSQLRepository
 from src.repositories.users import UsersPostgreSQLRepository
 
 
@@ -14,6 +15,7 @@ class DBManager:
         self.roles = RolesPostgreSQLRepository(self.session)
         self.permissions = PermissionsPostgreSQLRepository(self.session)
         self.oauth_accounts = OAuthAccountsPostgreSQLRepository(self.session)
+        self.subscriptions = SubscriptionsPostgreSQLRepository(self.session)
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
