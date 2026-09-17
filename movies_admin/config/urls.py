9e3eadbba46aab7240e069cffb54f459f575c70b
DaryaRu <fonarina57@gmail.com>
2026-09-17 13:50:00 +0300

    feat: change login view on admin_login view

--- movies_admin/config/urls.py
+++ movies_admin/config/urls.py
@@ -1,11 +1,17 @@
+from core.views import admin_login
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+# Подменяем view логина сайта админки на свою с сдвухшаговым входом:
+# пароль, затем код из СМС при включенной 2FA.
+admin.site.login = admin_login
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
     from debug_toolbar.toolbar import debug_toolbar_urls
+
     urlpatterns += debug_toolbar_urls()
